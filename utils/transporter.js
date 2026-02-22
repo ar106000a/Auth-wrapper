debebbd54896ffb7218efb23ba2ccb4c94988635
ar106000a <ar1062000a@gmail.com>
2026-02-22 14:20:57 +0500

    fix the port and other flags of transporter

--- utils/transporter.js
+++ utils/transporter.js
@@ -1,8 +1,11 @@
 import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   service: "gmail",
+  secure: false,
+  port: 587,
   auth: {
     user: process.env.TRANSPORTER_EMAIL,
     pass: process.env.TRANSPORTER_PASS, // not your Gmail password, but an "App password"
   },
+  connectionTimeout: 10000, // 10 seconds
 });
