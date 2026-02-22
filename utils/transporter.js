e015c8e0a0fd615b6e9bb3a6c29661546c758b48
ar106000a <ar1062000a@gmail.com>
2026-02-22 14:32:38 +0500

    try a different transporter flags

--- utils/transporter.js
+++ utils/transporter.js
@@ -1,11 +1,15 @@
 import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   service: "gmail",
-  secure: false,
-  port: 587,
+  host: "smtp.gmail.com", // Add host explicitly
+  port: 465, // Try switching to 465
+  secure: true, // Must be true for 465
   auth: {
     user: process.env.TRANSPORTER_EMAIL,
     pass: process.env.TRANSPORTER_PASS, // not your Gmail password, but an "App password"
   },
   connectionTimeout: 10000, // 10 seconds
+  tls: {
+    rejectUnauthorized: false, // Helps with some cloud certificate issues
+  },
 });
