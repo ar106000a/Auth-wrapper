b36d19aa1d3d8686e2fef490aaebf1fcc72ba3af
ar106000a <ar1062000a@gmail.com>
2026-02-22 18:06:06 +0500

    add readme and db schema

--- db/db.sql
+++ db/db.sql
@@ -1,12 +1,39 @@
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  username VARCHAR(50) UNIQUE NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL,
-  password_hash TEXT NOT NULL,
-  first_name VARCHAR(100),
-  last_name VARCHAR(100),
-  avatar_url TEXT,
-  is_verified BOOLEAN DEFAULT FALSE,
-  created_at TIMESTAMP DEFAULT NOW(),
-  updated_at TIMESTAMP DEFAULT NOW()
-);
+create table public.otps (
+  id uuid not null default extensions.uuid_generate_v4 (),
+  user_id uuid null,
+  otp_code text not null,
+  created_at timestamp with time zone null default now(),
+  expires_at timestamp with time zone not null default (now() + '00:10:00'::interval),
+  purpose text not null default 'email_verification'::text,
+  used boolean null default false,
+  constraint otps_pkey primary key (id),
+  constraint otps_user_id_fkey foreign KEY (user_id) references users (id) on delete CASCADE
+) TABLESPACE pg_default;
+
+create index IF not exists idx_otps_user_id on public.otps using btree (user_id) TABLESPACE pg_default;
+
+create table public.users (
+  id uuid not null default extensions.uuid_generate_v4 (),
+  email text not null,
+  password_hash text not null,
+  username text not null,
+  first_name text null,
+  last_name text null,
+  is_verified boolean null default false,
+  created_at timestamp with time zone null default now(),
+  updated_at timestamp with time zone null default now(),
+  avatar_url text null,
+  github_connected boolean null default false,
+  points bigint null default '0'::bigint,
+  rank character varying null default 'Newbie'::character varying,
+  is_test_user boolean null default false,
+  constraint users_pkey primary key (id),
+  constraint users_email_key unique (email),
+  constraint users_username_key unique (username)
+) TABLESPACE pg_default;
+
+create unique INDEX IF not exists users_username_idx on public.users using btree (lower(username)) TABLESPACE pg_default;
+
+create trigger trg_update_users_updated_at BEFORE
+update on users for EACH row
+execute FUNCTION update_users_updated_at ();
